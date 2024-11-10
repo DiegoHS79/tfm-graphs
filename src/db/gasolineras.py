a7850df13955d47ac1844c802a0f69a9754dd421
DiegoHS79 <diego.hidalgo@hotmail.es>
2024-11-10 08:06:00 +0100

    🚀 created gasolineras mongo.

---
+++ src/db/gasolineras.py
@@ -0,0 +1,130 @@
+import ast
+import os
+import random
+import re
+import sys
+import time
+from pathlib import Path
+from pprint import pp
+from math import floor, ceil
+
+import numpy as np
+import pandas as pd
+from pandas import Series
+
+from utils.mongo_db import MongoDBConnect
+from utils.geo_coords import get_coordinates
+
+os.system("clear")
+pd.options.mode.copy_on_write = True
+
+# LAT y LONG de las comunidades
+GEO_COM = Path("data/listado-longitud-latitud-municipios-espana.xls")
+GEO_MAX = Path("data/coords_max.csv")
+GEO_MIN = Path("data/coords_min.csv")
+if not GEO_MAX.exists() or not GEO_MIN.exists():
+    df_com = pd.read_excel(GEO_COM, sheet_name="Hoja1", header=2)
+    df_com["Provincia"] = df_com["Provincia"].apply(lambda x: x.lower())
+    df = df_com.loc[:, ["Provincia", "Latitud", "Longitud"]]
+
+    df_max = df.groupby(["Provincia"]).max()
+    df_max.to_csv(GEO_MAX, sep="\t")
+    df_min = df.groupby(["Provincia"]).min()
+    df_min.to_csv(GEO_MIN, sep="\t")
+else:
+    df_max = pd.read_csv(GEO_MAX, sep="\t")
+    df_max.set_index("Provincia", inplace=True)
+    df_min = pd.read_csv(GEO_MIN, sep="\t")
+    df_min.set_index("Provincia", inplace=True)
+
+
+def normalize_data(sf: Series) -> dict:
+    direction = sf.loc["dirección"].lower()
+    prov = sf.loc["provincia"].lower()
+
+    if prov == "coruña (a)":
+        prov = "a coruña"
+    elif prov == "araba/álava":
+        prov = "álava"
+    elif prov == "rioja (la)":
+        prov = "la rioja"
+    elif prov == "palmas (las)":
+        prov = "las palmas"
+    elif prov == "alicante":
+        prov = "alicante/alacant"
+    elif prov == "castellón / castelló":
+        prov = "castellón/castelló"
+    elif prov == "valencia / valència":
+        prov = "valencia/valència"
+    elif prov == "balears (illes)":
+        prov = "illes balears"
+    elif prov == "bizkaia":
+        prov = "vizcaya"
+    elif prov == "gipuzkoa":
+        prov = "guipúzcoa"
+
+    max_coor = df_max.loc[[prov]].to_dict()
+    min_coor = df_min.loc[[prov]].to_dict()
+    lat_edges = (max_coor["Latitud"][prov], min_coor["Latitud"][prov])
+    lon_edges = (max_coor["Longitud"][prov], min_coor["Longitud"][prov])
+
+    print("-" * 100)
+    cp = str(int(sf.loc["c.p."])).zfill(5)
+    direction = f"{direction}, {sf.loc['municipio']}, {cp}, ({prov})"
+    lat = ast.literal_eval(sf.loc["latitud"].replace(",", "."))
+    long = ast.literal_eval(sf.loc["longitud (wgs84)"].replace(",", "."))
+
+    if (
+        lat > ceil(lat_edges[0])
+        or lat < floor(lat_edges[1])
+        or long > ceil(lon_edges[0])
+        or long < floor(lon_edges[1])
+    ):
+        print(f"\tdireccion: {direction}")
+        resp = get_coordinates(direction, lat_edges=lat_edges, long_edges=lon_edges)
+
+        print(f"INFO - nuevas coordenadas: {resp}")
+
+        sf.loc["latitud"] = resp[0]
+        sf.loc["longitud (wgs84)"] = resp[1]
+
+        time.sleep(1.5 + random.random())
+    else:
+        print("INFO - No hace faltan calcular nuevas coordenadas.")
+
+    sf.rename({"longitud (wgs84)": "longitud"}, inplace=True)
+
+    return sf.to_dict()
+
+
+GASOLINERAS = Path("../scrapy_data/spiders/data/gasolineras_v1.csv")
+GASOLINERAS_FINAL = Path("data/gasolineras.csv")
+if not GASOLINERAS_FINAL.exists():
+    df = pd.read_csv(GASOLINERAS, sep="\t")
+    for i in range(df.shape[0]):
+        print(f"{i} - ", end="")
+        document = normalize_data(df.iloc[i, :])
+        # break
+        if i == 0:
+            df_final = pd.DataFrame.from_dict(document, orient="index").T
+        elif i > 0:
+            new_row = pd.DataFrame.from_dict(document, orient="index").T
+            df_final = pd.concat([df_final, new_row])
+
+    df_final.to_csv(GASOLINERAS_FINAL, index=False, sep="\t")
+
+
+# if GASOLINERAS_FINAL.exists:
+#     conn = MongoDBConnect(container_name="mongo-tfm", database="tfm-data")
+#     col = conn.collection(
+#         name="comisarias_cnp",
+#         mode="create",
+#         # delete=True,
+#     )
+#     df_tmp = pd.read_csv(GASOLINERAS_FINAL, sep="\t")
+#     for j in range(df_tmp.shape[0]):
+#         document = df_tmp.iloc[j, :].to_dict()
+#         conn.insert(collection=col, data=document)
+
+#     conn.db_manage(list_collections=True)
+#     conn.close()
